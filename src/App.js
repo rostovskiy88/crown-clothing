59dc6d505482e4a901dd7d189b97e6e97771abfd
Oleg Rostovskyi <rostovskiyoleg88@gmail.com>
2021-08-12 10:55:43 +0300

    xdfsg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <HomePage />
       </div>
     );
