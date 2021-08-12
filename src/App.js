f12e2d003e7a8b79a8e19f351aad8c3efb347722
Oleg Rostovskyi <rostovskiyoleg88@gmail.com>
2021-08-12 10:50:04 +0300

    fgctyhjfcghj

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+import HomePage from './homepage.component';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const MenuItem = ({ title }) => (
-  <div className="menu-item">
-    <div className="content">
-      <div className="title">{title}</div>
-      <span className="subtitle">SHOP NOW</span>
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
 );
