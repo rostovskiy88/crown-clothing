672167e4a48087593968adaef79e86ecb906883a
Oleg Rostovskyi <rostovskiyoleg88@gmail.com>
2021-08-13 17:26:44 +0300

    cfgh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import HomePage from './pages/homepage/homepage.component';
-
 import './App.css';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -16,7 +15,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/collections-preview/collection-preview.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './collection-preview.styles.scss';
+
+import CollectionItem from '../collection-item/collection-item.component';
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-prewiev">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="prewiev">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
